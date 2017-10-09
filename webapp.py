2c6d8722b1583e8599e2c36ec64ae120b03f3cc6
michaelmoschitto <mikemoschitto@gmail.com>
2017-10-09 09:32:28 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,11 +8,14 @@ def render_main():
 
 @app.route("/response")
 def render_response():
-    color = request.args['color']
+    color = request.args['color'] #the request object stores the information about the request sent to the server.
+            #the args field is a multi dict which is like a dict except it can have multiple values for the same key.
+            # the inforation in args ifs visible in the url for the page being requested (ex... /response?color=blue)
     if color == 'pink':
         reply = "Thats my favorite color too!"
     else:
         reply = "My favorite color is pink."
+    return render_template('response.html', response = reply)
     
     
 if __name__=="__main__":
